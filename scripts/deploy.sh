6146498cacea6108432d82cbbee216157edf6e7b
dandyhuang <vivo@tx.com>
2021-06-21 17:21:52 +0800

    add

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 rm -rf bin.tar.gz
+# 参数要大于1个
+if [ $# -lt 1 ]; then
+    echo "Usage: rank4_cron "
+    exit
+fi
 tar zcvf bin.tar.gz config scripts bin/${1}
 echo "应用名：$1"
 sh batch_command.sh machine/iplist.txt "sudo mkdir /data/${1}; cd /data/${1} && sudo chown rtrs:rtrs -R /data/${1} &&  mv bin.tar.gz binbak.tar.gz" |sh
